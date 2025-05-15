88d52dd8e6745c68d2841402518234c8e0b7cc50
Alvaro Rincon <alvarorinconc1@gmail.com>
2025-05-14 20:43:39 -0500

    Mostrar lista de frases.
    - Se crea la función conposable "FraseItem" para mostrar la lista de frases.
    - Se implementa la función conposable "FraseItem" en un "LazyColumn".

--- app/src/main/java/com/example/mindfrase/presentation/ListaFrases.kt
+++ app/src/main/java/com/example/mindfrase/presentation/ListaFrases.kt
@@ -2,6 +2,11 @@ package com.example.mindfrase.presentation
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -10,7 +15,10 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mindfrase.domain.model.Frase
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -28,5 +36,27 @@ fun ListaFrases(
         TopAppBar(
             title = { Text(text = "Mis frases") }
         )
+        LazyColumn {
+            itemsIndexed(frases) { index, frase ->
+                FraseItem(frase)
+            }
+        }
+    }
+}
+
+@Composable
+fun FraseItem(frase: Frase) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(text = frase.texto, fontSize = 18.sp)
+        }
     }
 }
