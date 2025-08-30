25edfd206d92a36cf36edec9f2f5f32d8eceea8a
Alvaro_Rincon <alvarorinconc1@gmail.com>
2025-08-29 22:04:57 -0500

    Crear nueva frase.
    - Se crea un campo de texto para escribir una nueva frase.
    - Se agerga un boton para guardar la nueva frase escrita.
    - Se guarda la nueva frase escrita.

--- app/src/main/java/com/example/mindfrase/presentation/NuevaFrase.kt
+++ app/src/main/java/com/example/mindfrase/presentation/NuevaFrase.kt
@@ -1,18 +1,32 @@
 package com.example.mindfrase.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.mindfrase.domain.model.Frase
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,6 +34,9 @@ fun NuevaFrase(
     navController: NavController,
     viewModel: FrasesViewModel = hiltViewModel()
 ) {
+    var textoFrase by remember {
+        mutableStateOf("")
+    }
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -33,5 +50,34 @@ fun NuevaFrase(
                     )
                 }
             }, title = { Text(text = "Nueva frase") })
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(
+                value = textoFrase,
+                onValueChange = { textoFrase = it },
+                label = {
+                    Text("Escribe una frase")
+                })
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                if (textoFrase.isNotBlank()) {
+                    val nuevaFrase = Frase(
+                        id = 0,
+                        texto = textoFrase,
+                        esFavorita = false,
+                        likes = 0,
+                    )
+                    viewModel.addFrase(nuevaFrase)
+                    navController.navigateUp()
+                }
+            }) {
+                Text("Guardar Frase")
+            }
+        }
     }
 }
